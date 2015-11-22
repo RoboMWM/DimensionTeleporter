56a5f652e410dc667bc5688d51bef94d3f93cc29
RoboMWM <robomwm@users.noreply.github.com>
2015-11-22 12:10:00 -0800

    Update to 1.2, removed .xml from .gitignore

--- src/main/java/me/robomwm/DimensionTeleporter/Main.java
+++ src/main/java/me/robomwm/DimensionTeleporter/Main.java
@@ -1,12 +1,14 @@
 package me.robomwm.DimensionTeleporter;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.util.ListIterator;
 
 /**
  * Created by Robo on 11/21/2015.
@@ -25,7 +27,14 @@ public class Main extends JavaPlugin
         if (cmd.getName().equalsIgnoreCase("dt"))
         {
             if (args.length < 1)
+            {
+                sender.sendMessage("Loaded worlds:");
+                ListIterator<World> worlds = Bukkit.getWorlds().listIterator();
+                while (worlds.hasNext())
+                    sender.sendMessage(worlds.next().getName());
                 return false;
+            }
+
             if (!(sender instanceof Player))
             {
                 sender.sendMessage("Use /dtp <world name> <player>");
@@ -41,7 +50,11 @@ public class Main extends JavaPlugin
                 World dest = Bukkit.getWorld(args[0]);
                 if (dest == null)
                 {
-                    sender.sendMessage("World " + args[0] + " is not loaded.");
+                    sender.sendMessage("Loaded worlds:");
+                    ListIterator<World> worlds = Bukkit.getWorlds().listIterator();
+                    while (worlds.hasNext())
+                        sender.sendMessage(worlds.next().getName());
+                    sender.sendMessage(ChatColor.RED + "World \"" + args[0] + "\" is not loaded.");
                     return false;
                 }
                 Location spawn = dest.getSpawnLocation();
@@ -52,7 +65,13 @@ public class Main extends JavaPlugin
         else if (cmd.getName().equalsIgnoreCase("dtp"))
         {
             if (args.length < 2)
+            {
+                sender.sendMessage(ChatColor.YELLOW + "Loaded worlds:");
+                ListIterator<World> worlds = Bukkit.getWorlds().listIterator();
+                while (worlds.hasNext())
+                    sender.sendMessage(ChatColor.GOLD + worlds.next().getName());
                 return false;
+            }
 //            if ((sender instanceof Player))
 //            {
 //                Player p = (Player)sender;
@@ -65,18 +84,22 @@ public class Main extends JavaPlugin
             Player player = (Bukkit.getPlayer(args[1]));
             if (player == null)
             {
-                sender.sendMessage("Player \"" + args[1] + "\" is not online.");
+                sender.sendMessage(ChatColor.RED + "Player \"" + args[1] + "\" is not online.");
                 return false;
             }
             World dest = Bukkit.getWorld(args[0]);
             if (dest == null)
             {
-                sender.sendMessage("World \"" + args[0] + "\" is not loaded.");
+                sender.sendMessage("Loaded worlds:");
+                ListIterator<World> worlds = Bukkit.getWorlds().listIterator();
+                while (worlds.hasNext())
+                    sender.sendMessage(worlds.next().getName());
+                sender.sendMessage(ChatColor.RED + "World \"" + args[0] + "\" is not loaded.");
                 return false;
             }
             Location spawn = dest.getSpawnLocation();
             player.teleport(spawn);
-            sender.sendMessage("Teleported " + player.getName() + " to " + dest.getName());
+            sender.sendMessage(ChatColor.GREEN + "Teleported " + player.getName() + " to " + dest.getName());
             return true;
         }
         return false;
